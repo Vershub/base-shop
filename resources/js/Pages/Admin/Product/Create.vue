4116a345e66f016da36fd6be9e236cb15f4c07cf
Andrey Kovtonyuk <andreykovtonyuk@Andreys-MacBook-Air.local>
2025-02-20 01:55:38 +0200

    feat: spatie simple excel package added

--- resources/js/Pages/Admin/Product/Create.vue
+++ resources/js/Pages/Admin/Product/Create.vue
@@ -1,12 +1,13 @@
 <template>
-<AdminLayout>
-  <Form :languages="languages"/>
-</AdminLayout>
+  <AdminLayout>
+    <h1 class="text-center mb-3 uppercase tracking-widest">Create Product</h1>
+    <Form :languages="languages"/>
+  </AdminLayout>
 </template>
 
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import Form from "@/Pages/Admin/Product/Bundle/Form.vue";
+import Form from "@/Pages/Admin/Product/Form.vue";
 
 defineProps({
   languages: {
