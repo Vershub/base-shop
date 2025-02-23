a4442d4ab505178ce7fd7fddecb729d46ced02a9
Vers <vers11@ukr.net>
2025-02-23 02:35:40 +0200

    feat: admin category delete

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -102,8 +102,10 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): void
+    public function destroy(string $id): RedirectResponse
     {
-        //
+        Category::findOrFail($id)->delete();
+
+        return to_route('admin.categories.index');
     }
 }
